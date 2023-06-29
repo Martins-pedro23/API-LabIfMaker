f60ace9c410d6c4a18531de3b14036ae8f1e201d
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-06-29 18:55:46 -0300

    feat: adding AdminModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const AdminSchema = new mongoose.Schema({
   id: { type: Number, required: true },
   name: String,
   email: { type: String, required: true, unique: true, lowCase: true },
   password: String,
 });
 
-export default mongoose.model<typeof UserSchema>("User", UserSchema);
+export default mongoose.model<typeof AdminSchema>("User", AdminSchema);
