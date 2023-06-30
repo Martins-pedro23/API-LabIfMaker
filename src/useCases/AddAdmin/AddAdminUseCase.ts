3b8374b9977a1d9f6f1f2d26ce4474409b1db412
Kyoudan <fogoste@gmail.com>
2023-06-29 21:00:59 -0300

    feat: adding cryptography on admin register

--- src/useCases/AddAdmin/AddAdminUseCase.ts
+++ src/useCases/AddAdmin/AddAdminUseCase.ts
@@ -2,18 +2,17 @@ import { AdminRepository } from "../../repositories/implementations/AdminReposit
 import { IAdminRepository } from "../../repositories/IAdminRepository";
 import { AddAdminDTO } from "./AddAdminDTO";
 import { Admin } from "../../entities/Admin";
+import * as bcrypt from "bcrypt";
 
 export class AddAdminUseCase {
   constructor(private readonly AdminRepository: IAdminRepository) {}
   async execute(admin: AddAdminDTO): Promise<Admin | undefined> {
     try {
-      if (!admin.name) throw new Error("Name is required");
-      if (!admin.email) throw new Error("Email is required");
-      if (!admin.password) throw new Error("Password is required");
-
       const alreadyExists = await this.AdminRepository.findByEmail(admin.email);
 
       if (alreadyExists) throw new Error("Admin already exists");
+
+      admin.password = bcrypt.hashSync(admin.password, 10);
       const result = await this.AdminRepository.addAdmin(admin);
 
       return result;
