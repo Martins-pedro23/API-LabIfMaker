64024ef93372b10262085022bedf0858b6998bd6
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-07-03 10:35:03 -0300

    feat: starting to add graphql

---
+++ src/resolvers.ts
@@ -0,0 +1,9 @@
+import { MyContext } from "./context";
+
+const resolvers = {
+	Query: {
+		helloWorld: (parent: any, args: any, context: MyContext, info: any) => context.greeting,
+	},
+};
+
+export default resolvers;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,29 @@ import { config } from "dotenv";
 config();
 import { green, cyan, magenta } from "colorette";
 import fastify, { FastifyInstance } from "fastify";
+import { ApolloServer, BaseContext } from "@apollo/server";
+import fastifyApollo, { fastifyApolloDrainPlugin } from "@as-integrations/fastify";
 import { Routes } from "./routes";
+import  typeDefs  from "./typeDefs";
+import resolvers from "./resolvers";
 import { connetion } from "./database/connection";
 
 const server: FastifyInstance = fastify({
   logger: false,
 });
+
+const apollo = new ApolloServer<BaseContext>({
+  typeDefs,
+  resolvers,
+  plugins: [fastifyApolloDrainPlugin(server)],
+});
+
+async () => {
+  await apollo.start();
+  await server.register(fastifyApollo(apollo));
+
+}
+
 
 Routes(server);
 
@@ -17,8 +34,9 @@ const start = async () => {
   try {
     await server.listen({ port: port, host: "0.0.0.0" });
     await connetion();
-
+    
     console.log("🚀 Server listening on port: " + cyan(port));
+    apollo.logger.info("🌟 ApolloServer is responding: " + green("true"));
   } catch (err) {
     server.log.error("Erro", err);
     process.exit(1);
